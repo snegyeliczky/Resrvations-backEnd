92693d3fdb7e1d43b781f510ab4935ba578f09d4
AkosCzifra <akosczifraa@gmail.com>
2020-01-07 15:47:27 +0100

    implemented roomStorage and roomController

--- src/main/java/com/codecool/reservationsbackend/service/RoomStorage.java
+++ src/main/java/com/codecool/reservationsbackend/service/RoomStorage.java
@@ -1,10 +1,13 @@
 package com.codecool.reservationsbackend.service;
 
 import com.codecool.reservationsbackend.model.Room;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class RoomStorage {
     private List<Room> roomList = new ArrayList<>();
 
@@ -12,9 +15,16 @@ public class RoomStorage {
         this.roomCreator = roomCreator;
     }
 
+    @Autowired
     private RoomCreator roomCreator;
 
-    public void addRoom() {
-        this.roomList.add(roomCreator.createRoom());
+    public Room addRoom() {
+        Room room = roomCreator.createRoom();
+        this.roomList.add(room);
+        return room;
+    }
+
+    public List<Room> getRoomList() {
+        return roomList;
     }
 }
