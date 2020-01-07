059e730ec6eacb5468ccc3c31033e0a3687e95b1
Sándor Negyeliczky <sandor.negyeliczky@gmail.com>
2020-01-07 11:13:36 +0100

    spring basic

---
+++ src/test/java/com/codecool/reservationsbackend/ReservationsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.reservationsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
