152bec4d72096d6190c5494dc257bd2afd894238
BotondSiklosi <kissiki99@gmai.com>
2020-02-20 11:10:47 +0100

    implemented new endpoint in roomcontroller

--- reservation-service/src/main/java/com/codecool/reservationsbackend/controller/RoomController.java
+++ reservation-service/src/main/java/com/codecool/reservationsbackend/controller/RoomController.java
@@ -4,12 +4,11 @@ import com.codecool.reservationsbackend.entity.Hotel;
 import com.codecool.reservationsbackend.entity.Room;
 import com.codecool.reservationsbackend.repositories.HotelRepository;
 import com.codecool.reservationsbackend.repositories.RoomRepository;
+import com.codecool.reservationsbackend.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @CrossOrigin
@@ -22,6 +21,14 @@ public class RoomController {
 
     @Autowired
     private HotelRepository hotelRepository;
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @GetMapping("/get-available-room")
+    public List<Room> getAvailableRoomsByDates(@RequestParam("checkin") LocalDate checkIn, @RequestParam("checkout") LocalDate checkOut) {
+        return reservationService.getAvailableRoomsByDates(checkIn, checkOut);
+    }
 
     @GetMapping("/get-all")
     public List<Room> roomList() {
